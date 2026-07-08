2a750f928f24d78fda42445b77cf9c3b123fe281
Francesco Palma <117196189+FrancescoPalmaDev@users.noreply.github.com>
2026-07-08 17:07:45 +0200

    main http logic

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"todo-http-app/todo"
+)
 
+var todoList = todo.ToDoList{FilePath: "todos.json"}
+
+func main() {
+	todoList.Load()
+	http.HandleFunc("/tasks", handleTasks)
+	http.HandleFunc("/tasks/", handleTasksById)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleTasks(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "GET":
+		for _, task := range todoList.Tasks {
+			fmt.Fprintf(writer, "%d - %s\n", task.Id, task.Title)
+		}
+	case "POST":
+		title := request.URL.Query().Get("title")
+		todoList.Add(title)
+		fmt.Fprintf(writer, "Task added: %s", title)
+	}
+}
+
+func handleTasksById(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "DELETE" {
+		stringId := request.URL.Path[len("/tasks/"):]
+		id, _ := strconv.Atoi(stringId)
+		todoList.Remove(id)
+		fmt.Fprintf(writer, "Task with Id: %d has been removed", id)
+	}
 }
